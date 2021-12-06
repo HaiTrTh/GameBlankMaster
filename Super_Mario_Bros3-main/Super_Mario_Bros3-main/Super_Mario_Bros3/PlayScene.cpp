85ca79d4c2b040aebc27e113c1428f0fd566e6df
HaiTrTh <haithanhtran00@gmail.com>
2021-12-06 09:02:24 +0700

    full enemy, bullet

--- Super_Mario_Bros3-main/Super_Mario_Bros3-main/Super_Mario_Bros3/PlayScene.cpp
+++ Super_Mario_Bros3-main/Super_Mario_Bros3-main/Super_Mario_Bros3/PlayScene.cpp
@@ -26,16 +26,106 @@ CPlayScene::CPlayScene(int id, LPCWSTR filePath) :
 #define SCENE_SECTION_ANIMATIONS 4
 #define SCENE_SECTION_ANIMATION_SETS	5
 #define SCENE_SECTION_OBJECTS	6
+#define SCENE_SECTION_MAP	7
+#define SCENE_SECTION_QUADTREE	8
+#define SCENE_SECTION_SETTING	9
 
-#define OBJECT_TYPE_MARIO	0
+#define OBJECT_TYPE_TANK_BODY	0
+#define OBJECT_TYPE_TANK_PART	100
 #define OBJECT_TYPE_BRICK	1
-#define OBJECT_TYPE_GOOMBA	2
-#define OBJECT_TYPE_KOOPAS	3
+#define OBJECT_TYPE_CTANKBULLET	2
+
 
 #define OBJECT_TYPE_PORTAL	50
 
 #define MAX_SCENE_LINE 1024
 
+void CPlayScene::Load()
+{
+	DebugOut(L"[INFO] Start loading scene resources from : %s \n", sceneFilePath);
+
+	ifstream f;
+	f.open(sceneFilePath);
+
+	// current resource section flag
+	int section = SCENE_SECTION_UNKNOWN;
+
+	char str[MAX_SCENE_LINE];
+	while (f.getline(str, MAX_SCENE_LINE))
+	{
+		string line(str);
+
+		if (line[0] == '#') continue;	// skip comment lines	
+		if (line == "[MAP]") { section = SCENE_SECTION_MAP; continue; }
+		if (line == "[TEXTURES]") { section = SCENE_SECTION_TEXTURES; continue; }
+		if (line == "[SPRITES]") {
+			section = SCENE_SECTION_SPRITES; continue;
+		}
+		if (line == "[ANIMATIONS]") {
+			section = SCENE_SECTION_ANIMATIONS; continue;
+		}
+		if (line == "[MAP]") { section = SCENE_SECTION_MAP; continue; }
+		if (line == "[ANIMATION_SETS]") {
+			section = SCENE_SECTION_ANIMATION_SETS; continue;
+		}
+		if (line == "[OBJECTS]") {
+			section = SCENE_SECTION_OBJECTS; continue;
+		}
+		if (line == "[QUADTREE]") {
+			section = SCENE_SECTION_QUADTREE; continue;
+		}
+		if (line == "[SETTING]") {
+			section = SCENE_SECTION_SETTING; continue;
+		}
+		if (line[0] == '[') { section = SCENE_SECTION_UNKNOWN; continue; }
+
+		//
+		// data section
+		//
+		switch (section)
+		{
+		case SCENE_SECTION_MAP: _ParseSection_MAP(line); break;
+		case SCENE_SECTION_SETTING: _ParseSection_SETTING(line); break;
+		case SCENE_SECTION_TEXTURES: _ParseSection_TEXTURES(line); break;
+		case SCENE_SECTION_SPRITES: _ParseSection_SPRITES(line); break;
+		case SCENE_SECTION_ANIMATIONS: _ParseSection_ANIMATIONS(line); break;
+		case SCENE_SECTION_ANIMATION_SETS: _ParseSection_ANIMATION_SETS(line); break;
+		case SCENE_SECTION_OBJECTS: _ParseSection_OBJECTS(line); break;
+		case SCENE_SECTION_QUADTREE: _ParseSection_QUADTREE(line); break;
+		}
+	}
+
+	f.close();
+
+	CTextures::GetInstance()->Add(ID_TEX_BBOX, L"textures\\bbox.png", D3DCOLOR_XRGB(255, 255, 255));
+
+	DebugOut(L"[INFO] Done loading scene resources %s\n", sceneFilePath);
+}
+
+void CPlayScene::_ParseSection_QUADTREE(string line)
+{
+	vector<string> tokens = split(line);
+
+	if (tokens.size() < 1) return;
+
+	wstring file_path = ToWSTR(tokens[0]);
+	if (quadtree == NULL)
+		quadtree = new CQuadTree(file_path.c_str());
+}
+
+void CPlayScene::_ParseSection_SETTING(string line)
+{
+	vector<string> tokens = split(line);
+
+	if (tokens.size() < 1) return;
+
+	setMapheight(int(atoi(tokens[0].c_str())));
+
+	CGame::GetInstance()->GetCurrentScene()->setMapheight(int(atoi(tokens[0].c_str())));
+
+	DebugOut(L"Y: la %d  \n", getMapheight());
+
+}
 
 void CPlayScene::_ParseSection_TEXTURES(string line)
 {
@@ -142,20 +232,30 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 
 	switch (object_type)
 	{
-	case OBJECT_TYPE_MARIO:
+	case OBJECT_TYPE_TANK_BODY:
 		if (player != NULL)
 		{
-			DebugOut(L"[ERROR] MARIO object was created before!\n");
+			DebugOut(L"[ERROR] TANK_BODY object was created before!\n");
 			return;
 		}
-		obj = new CMario(x, y);
-		player = (CMario*)obj;
+		obj = new CTANK_BODY(x, getMapheight() - y);
+		//obj = new CTANK_BODY(x,y);
+		DebugOut(L"RRRRRRRRRrY Xe la %d  %f  \n", getMapheight(),y);
+
+		player = (CTANK_BODY*)obj;
 
 		DebugOut(L"[INFO] Player object created!\n");
+
 		break;
-	case OBJECT_TYPE_GOOMBA: obj = new CGoomba(); break;
 	case OBJECT_TYPE_BRICK: obj = new CBrick(); break;
-	case OBJECT_TYPE_KOOPAS: obj = new CKoopas(); break;
+	case OBJECT_TYPE_CTANKBULLET: obj = new CTANKBULLET(); break;
+		
+	case OBJECT_TYPE_TANK_PART:
+	{
+		float part = atof(tokens[4].c_str());
+		obj = new TankParts(part);
+	}
+	break;
 	case OBJECT_TYPE_PORTAL:
 	{
 		float r = atof(tokens[4].c_str());
@@ -164,78 +264,107 @@ void CPlayScene::_ParseSection_OBJECTS(string line)
 		obj = new CPortal(x, y, r, b, scene_id);
 	}
 	break;
+	
 	default:
 		DebugOut(L"[ERR] Invalid object type: %d\n", object_type);
 		return;
 	}
 
 	// General object setup
-	obj->SetPosition(x, y);
+
 
 	LPANIMATION_SET ani_set = animation_sets->Get(ani_set_id);
 
-	obj->SetAnimationSet(ani_set);
-	objects.push_back(obj);
+	if (obj != NULL )
+	{
+		if(object_type != OBJECT_TYPE_TANK_BODY)
+		obj->SetPosition(x, getMapheight() - y);
+		obj->SetAnimationSet(ani_set);
+		obj->SetOrigin(x, y, obj->GetState());
+		obj->SetisOriginObj(true);
+		objects.push_back(obj);
+	}
 }
 
-void CPlayScene::Load()
+void CPlayScene::_ParseSection_MAP(string line)
 {
-	DebugOut(L"[INFO] Start loading scene resources from : %s \n", sceneFilePath);
+	vector<string> tokens = split(line);
 
-	ifstream f;
-	f.open(sceneFilePath);
+	if (tokens.size() < 7) return; // skip invalid lines
 
-	// current resource section flag
-	int section = SCENE_SECTION_UNKNOWN;
+	int idTileSet = atoi(tokens[0].c_str());
+	int totalRowsTileSet = atoi(tokens[1].c_str());
+	int totalColumnsTileSet = atoi(tokens[2].c_str());
+	int totalRowsMap = atoi(tokens[3].c_str());
+	int totalColumnsMap = atoi(tokens[4].c_str());
+	int totalTiles = atoi(tokens[5].c_str());
+	wstring file_path = ToWSTR(tokens[6]);
 
-	char str[MAX_SCENE_LINE];
-	while (f.getline(str, MAX_SCENE_LINE))
-	{
-		string line(str);
+	map = new Map(idTileSet, totalRowsTileSet, totalColumnsTileSet, totalRowsMap, totalColumnsMap, totalTiles);
+	map->LoadMap(file_path.c_str());
+	map->ExtractTileFromTileSet();
+}
 
-		if (line[0] == '#') continue;	// skip comment lines	
+bool CPlayScene::IsInUseArea(float Ox, float Oy)
+{
+	float CamX, CamY;
 
-		if (line == "[TEXTURES]") { section = SCENE_SECTION_TEXTURES; continue; }
-		if (line == "[SPRITES]") {
-			section = SCENE_SECTION_SPRITES; continue;
-		}
-		if (line == "[ANIMATIONS]") {
-			section = SCENE_SECTION_ANIMATIONS; continue;
-		}
-		if (line == "[ANIMATION_SETS]") {
-			section = SCENE_SECTION_ANIMATION_SETS; continue;
-		}
-		if (line == "[OBJECTS]") {
-			section = SCENE_SECTION_OBJECTS; continue;
-		}
-		if (line[0] == '[') { section = SCENE_SECTION_UNKNOWN; continue; }
+	CamX = (float)CGame::GetInstance()->GetCam().GetCamX();
 
-		//
-		// data section
-		//
-		switch (section)
-		{
-		case SCENE_SECTION_TEXTURES: _ParseSection_TEXTURES(line); break;
-		case SCENE_SECTION_SPRITES: _ParseSection_SPRITES(line); break;
-		case SCENE_SECTION_ANIMATIONS: _ParseSection_ANIMATIONS(line); break;
-		case SCENE_SECTION_ANIMATION_SETS: _ParseSection_ANIMATION_SETS(line); break;
-		case SCENE_SECTION_OBJECTS: _ParseSection_OBJECTS(line); break;
-		}
-	}
+	CamY = (float)CGame::GetInstance()->GetCam().GetCamY();
 
-	f.close();
-
-	CTextures::GetInstance()->Add(ID_TEX_BBOX, L"textures\\bbox.png", D3DCOLOR_XRGB(255, 255, 255));
-
-	DebugOut(L"[INFO] Done loading scene resources %s\n", sceneFilePath);
+	if (((CamX - CAM_X_BONUS < Ox) && (Ox < CamX + IN_USE_WIDTH)) && ((CamY < Oy) && (Oy < CamY + IN_USE_HEIGHT)))
+		return true;
+	return false;
 }
 
 void CPlayScene::Update(DWORD dt)
 {
-	// We know that Mario is the first object in the list hence we won't add him into the colliable object list
+	// We know that TANK_BODY is the first object in the list hence we won't add him into the colliable object list
 	// TO-DO: This is a "dirty" way, need a more organized way 
 
+	// skip the rest if scene was already unloaded (TANK_BODY::Update might trigger PlayScene::Unload)
+	if (player == NULL) return;
+
+	// Update camera to follow mario
+	float cx, cy;
+
+	player->GetPosition(cx, cy);
+
+	DebugOut(L"PST x y %f %f", cx, cy);
+
+	cy = cy;
+
+	/*DebugOut(L"Y: la %d %f  \n", CGame::GetInstance()->GetCurrentScene()->getMapheight(), cy);*/
+	
+	CGame* game = CGame::GetInstance();
+
+	cx -= game->GetScreenWidth() / 2;
+	cy -= game->GetScreenHeight() / 2;
+
+	if (cx < 0)
+	{
+		cx = 0;
+	}
+
+	CGame::GetInstance()->SetCamPos(cx, cy);
+
 	vector<LPGAMEOBJECT> coObjects;
+
+	quadtree->GetObjects(objects, (int)cx, (int)cy);
+
+	for (size_t i = 0; i < objects.size(); i++)
+	{
+		float Ox, Oy;
+		objects[i]->GetPosition(Ox, Oy);
+		if (!IsInUseArea(Ox, Oy) && !objects[i]->GetisOriginObj())
+		{
+			objects[i]->SetActive(false);
+			objects.erase(objects.begin() + i);
+			i--;
+		}
+	}
+
 	for (size_t i = 1; i < objects.size(); i++)
 	{
 		coObjects.push_back(objects[i]);
@@ -245,23 +374,16 @@ void CPlayScene::Update(DWORD dt)
 	{
 		objects[i]->Update(dt, &coObjects);
 	}
-
-	// skip the rest if scene was already unloaded (Mario::Update might trigger PlayScene::Unload)
-	if (player == NULL) return;
-
-	// Update camera to follow mario
-	float cx, cy;
-	player->GetPosition(cx, cy);
-
-	CGame* game = CGame::GetInstance();
-	cx -= game->GetScreenWidth() / 2;
-	cy -= game->GetScreenHeight() / 2;
-
-	CGame::GetInstance()->SetCamPos(cx, 0.0f /*cy*/);
 }
 
 void CPlayScene::Render()
 {
+	CGame* game = CGame::GetInstance();
+
+	if (map)
+	{
+		this->map->Render();
+	}
 	for (int i = 0; i < objects.size(); i++)
 		objects[i]->Render();
 }
@@ -284,29 +406,48 @@ void CPlayScenceKeyHandler::OnKeyDown(int KeyCode)
 {
 	//DebugOut(L"[INFO] KeyDown: %d\n", KeyCode);
 
-	CMario* mario = ((CPlayScene*)scence)->GetPlayer();
+	CTANK_BODY* mario = ((CPlayScene*)scence)->GetPlayer();
 	switch (KeyCode)
 	{
 	case DIK_SPACE:
-		mario->SetState(MARIO_STATE_JUMP);
+		mario->SetState(TANK_BODY_STATE_JUMP);
 		break;
-	case DIK_A:
+	case DIK_B:
 		mario->Reset();
 		break;
+	case DIK_A:
+		mario->SetisFiring(true);
+		break;
 	}
+}
+
+void CPlayScenceKeyHandler::OnKeyUp(int KeyCode)
+{
+	CTANK_BODY* mario = ((CPlayScene*)scence)->GetPlayer();
+		switch (KeyCode)
+		{
+		case DIK_A:
+			mario->SetisFiring(false);
+			mario->SetisAlreadyFired(false);
+			break;
+		}
 }
 
 void CPlayScenceKeyHandler::KeyState(BYTE* states)
 {
 	CGame* game = CGame::GetInstance();
-	CMario* mario = ((CPlayScene*)scence)->GetPlayer();
+	CTANK_BODY* mario = ((CPlayScene*)scence)->GetPlayer();
 
-	// disable control key when Mario die 
-	if (mario->GetState() == MARIO_STATE_DIE) return;
+	// disable control key when TANK_BODY die 
+	if (mario->GetState() == TANK_BODY_STATE_DIE) return;
 	if (game->IsKeyDown(DIK_RIGHT))
-		mario->SetState(MARIO_STATE_WALKING_RIGHT);
+		mario->SetState(TANK_BODY_STATE_WALKING_RIGHT);
 	else if (game->IsKeyDown(DIK_LEFT))
-		mario->SetState(MARIO_STATE_WALKING_LEFT);
+		mario->SetState(TANK_BODY_STATE_WALKING_LEFT);
+	else if (game->IsKeyDown(DIK_DOWN))
+		mario->SetState(TANK_BODY_STATE_WALKING_DOWN);
+	else if (game->IsKeyDown(DIK_UP))
+		mario->SetState(TANK_BODY_STATE_WALKING_UP);
 	else
-		mario->SetState(MARIO_STATE_IDLE);
+		mario->SetState(TANK_BODY_STATE_IDLE);
 }
--- Super_Mario_Bros3-main/Super_Mario_Bros3-main/Super_Mario_Bros3/PlayScene.h
+++ Super_Mario_Bros3-main/Super_Mario_Bros3-main/Super_Mario_Bros3/PlayScene.h
@@ -4,25 +4,88 @@
 #include "Scene.h"
 #include "GameObject.h"
 #include "Brick.h"
-#include "Mario.h"
-#include "Goomba.h"
+#include "TANK_BODY.h"
+#include "Eye.h"
 #include "Koopas.h"
+#include "Map.h"
+#include "TankParts.h"
+#include "MapObj.h"
+#include "CLaserGuard.h"
+#include "CBallCarry.h"
+#include "CBallbot.h"
+#include "CDrap.h"
+#include "CGX680.h"
+#include "CGX680S.h"
+#include "CSTUKA.h"
+#include "Eyelet.h"
+#include "Interrupt.h"
+#include "CTANKBULLET.h"
 
+#include "Utils.h"
+#include "Game.h"
+#include <iostream>
+#include <fstream>
+
+
+#define QUADTREE_SECTION_SETTINGS	1
+#define QUADTREE_SECTION_OBJECTS	2
+#define MAX_QUADTREE_LINE 1024
+
+class CQuadTree
+{
+	bool isLeaf = false;
+	float x = 0;
+	float y = 0;
+	float cellWidth = 0;
+	float  cellHeight = 0;
+	CQuadTree* BrachTL = NULL;
+	CQuadTree* BrachTR = NULL;
+	CQuadTree* BrachBL = NULL;
+	CQuadTree* BrachBR = NULL;
+	MapObj* obj;
+	vector<LPGAMEOBJECT> listObjects;
+
+	void _ParseSection_SETTINGS(string line);
+	void _ParseSection_OBJECTS(string line);
+	void _ParseSection_MapObj(string line);
+public:
+	CQuadTree(float cellWidth, float cellHeight, float x, float y);
+	CQuadTree(LPCWSTR filePath);
+	void GetObjects(vector<LPGAMEOBJECT>& listObject, int playerX, int playerY);
+	void Load(LPCWSTR filePath);
+	void Unload();
+	void Add(CGameObject* obj)
+	{
+		listObjects.push_back(obj);
+	}
+	int getVollunm(){
+		return listObjects.size();
+	}
+	void Render();
+	void Plit();
+	void ObjectPlit(CQuadTree* brach);
+	bool inRange(float ox, float oy, float x, float y, float width, float height);
+	void Pop(vector<LPGAMEOBJECT>& listObject, int CamX, int CamY);
+
+};
 
 class CPlayScene : public CScene
 {
 protected:
-	CMario* player;					// A play scene has to have player, right? 
-
+	CTANK_BODY* player;					// A play scene has to have player, right? 
 	vector<LPGAMEOBJECT> objects;
+	int mapHeight;
+	Map* map;
+	CQuadTree* quadtree;
 
 	void _ParseSection_TEXTURES(string line);
 	void _ParseSection_SPRITES(string line);
 	void _ParseSection_ANIMATIONS(string line);
 	void _ParseSection_ANIMATION_SETS(string line);
 	void _ParseSection_OBJECTS(string line);
-
-
+	void _ParseSection_MAP(string line);
+	void _ParseSection_QUADTREE(string line);
+	void _ParseSection_SETTING(string line);
 public:
 	CPlayScene(int id, LPCWSTR filePath);
 
@@ -31,7 +94,19 @@ public:
 	virtual void Render();
 	virtual void Unload();
 
-	CMario* GetPlayer() { return player; }
+	bool IsInUseArea(float Ox, float Oy);
+
+	CTANK_BODY* GetPlayer() { return player; }
+
+	void setMapheight(int height)
+	{
+		mapHeight = height;
+	}
+
+	int getMapheight()
+	{
+		return mapHeight;
+	}
 
 	//friend class CPlayScenceKeyHandler;
 };
@@ -41,7 +116,7 @@ class CPlayScenceKeyHandler : public CScenceKeyHandler
 public:
 	virtual void KeyState(BYTE* states);
 	virtual void OnKeyDown(int KeyCode);
-	virtual void OnKeyUp(int KeyCode) {};
+	virtual void OnKeyUp(int KeyCode);
 	CPlayScenceKeyHandler(CScene* s) :CScenceKeyHandler(s) {};
 };
 
